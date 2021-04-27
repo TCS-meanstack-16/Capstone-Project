940e132578a1796276dc76f22063edb2fb94dc0d
mvaldez2 <miguel.valdez@valpo.edu>
2021-04-27 16:35:23 -0500

    routing components

--- Frontend/product-grocers/src/app/app.module.ts
+++ Frontend/product-grocers/src/app/app.module.ts
@@ -19,14 +19,11 @@ import { ProfileInfoComponent } from './profile-info/profile-info.component';
 import { SendRequestComponent } from './send-request/send-request.component';
 import { UpdateOrderComponent } from './update-order/update-order.component';
 import { ViewTicketsComponent } from './view-tickets/view-tickets.component';
-
 import { SignUpComponent } from './sign-up/sign-up.component';
-
 import { UpdateProductComponent } from './product-components/update-product/update-product.component';
 import { DeleteProductComponent } from './product-components/delete-product/delete-product.component';
 import { AddProductComponent } from './product-components/add-product/add-product.component';
 import { RetrieveProductsComponent } from './product-components/retrieve-products/retrieve-products.component';
-import { UserComponent } from './user/user.component';
 
 
 @NgModule({
@@ -51,13 +48,27 @@ import { UserComponent } from './user/user.component';
     DeleteProductComponent,
     AddProductComponent,
     RetrieveProductsComponent,
-    UserComponent
 
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      {path: 'user', component: UserComponent},
+      {path: 'retrieve-products', component: RetrieveProductsComponent},
+      {path: 'add-product', component: AddProductComponent},
+      {path: 'delete-product', component: DeleteProductComponent},
+      {path: 'update-product', component: UpdateProductComponent},
+      {path: 'sign-up', component: SignUpComponent},
+      {path: 'view-tickets', component: ViewTicketsComponent},
+      {path: 'employee-ticket', component: EmployeeTicketComponent},
+      {path: 'raise-ticket', component: RaiseTicketComponent},
+      {path: 'view-requests', component: ViewRequestsComponent},
+      {path: 'send-requests', component: SendRequestComponent},
+      {path: 'profile-info', component: ProfileInfoComponent},
+      {path: 'orders', component: OrdersComponent},
+      {path: 'cart', component: CartComponent},
+      {path: 'reports', component: ReportsComponent},
+      {path: 'edit-employees', component: EditEmployeesComponent},
+      {path: 'login-page', component: LoginPageComponent},
       {path: 'update-order', component: UpdateOrderComponent}
     ]),
     AppRoutingModule,
