fa2b75e5cc0aed9a08de62d12f61c9ad0f35f005
irhshadow <47831479+irhshadow@users.noreply.github.com>
2021-04-27 13:05:28 -0400

    Added backend support for request functionality

--- Backend/router/employee.router.js
+++ Backend/router/employee.router.js
@@ -7,5 +7,8 @@ router.get("/retrieveEmployeeById/:eid",EmployeeController.getEmployeeById)
 router.post("/createEmployee",EmployeeController.createEmployee);
 router.put("/changePassword",EmployeeController.changePassword)
 router.delete("/deleteEmployeeById/:eid",EmployeeController.deleteEmployeeById);
+router.post("/createRequest", EmployeeController.createRequest);
+router.delete("/resolveRequest/:pid", EmployeeController.resolveRequest);
+router.get("/viewRequests",EmployeeController.getRequests)
 
 module.exports=router;
