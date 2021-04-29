cad1eabe3dd5b4ad73740e88f1c7da5ad7287dae
irhshadow <47831479+irhshadow@users.noreply.github.com>
2021-04-29 15:59:19 -0400

    Added signout component

--- Frontend/product-grocers/src/app/shared/nav/nav.component.ts
+++ Frontend/product-grocers/src/app/shared/nav/nav.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
+  userID;
+  isLoggedIn = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.userID = JSON.parse((localStorage.getItem("userId")));
+    if(this.userID.toString() != ""){
+      this.isLoggedIn = true;
+    }
+  }
+
+  logout(){
+    console.log("In signout");
+    localStorage.setItem("userId","")
   }
 
 }
