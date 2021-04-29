8e1616ec1e6768b6ac7b58dd7d518b6c2900d5d6
irhshadow <47831479+irhshadow@users.noreply.github.com>
2021-04-29 10:46:15 -0400

    Added functionality for showing user only their relevant page

--- Frontend/product-grocers/src/app/app.component.ts
+++ Frontend/product-grocers/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'product-grocers';
+  userID:number;
+  isAdmin:boolean = false;
+
+  constructor(public empSer: EmployeeService){
+
+  }
+
+  ngOnInit(): void {
+    this.userID = JSON.parse((localStorage.getItem("userId")));
+    let employee = this.empSer.getEmployee(this.userID).subscribe(employee =>{
+      this.isAdmin = employee[0].isAdmin;
+      console.log(this.isAdmin);
+    });
+    console.log(employee[0]);
+  }
+
 }
