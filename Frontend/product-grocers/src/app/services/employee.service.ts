13a726181af0b669e56363a381b2445bdddb1315
mvaldez2 <miguel.valdez@valpo.edu>
2021-04-29 16:16:30 -0500

    deployment files

--- Frontend/product-grocers/src/app/services/employee.service.ts
+++ Frontend/product-grocers/src/app/services/employee.service.ts
@@ -10,24 +10,24 @@ export class EmployeeService {
   
 //post method 1st parameter url and 2nd parameter json data. 
   createEmployee(employeeRef:any){
-    this.http.post("http://localhost:9090/employee/createEmployee",employeeRef,{responseType:"text"}).
+    this.http.post("http://100.26.50.117:9090/employee/createEmployee",employeeRef,{responseType:"text"}).
     subscribe(result=>console.log(result),error=>console.log(error));
   }
 
   getEmployee(id:any):Observable<Employee[]>{
-     return this.http.get<Employee[]>("http://localhost:9090/employee/retrieveEmployeeById/"+id)    //Should return array of tickets that can then be used to display tickets
+     return this.http.get<Employee[]>("http://100.26.50.117:9090/employee/retrieveEmployeeById/"+id)    //Should return array of tickets that can then be used to display tickets
   }
 
   //by default all HttpClient method return type is observable with json format data. 
   deleteEmployee(id:any):any{
-    return this.http.delete("http://localhost:9090/employee/deleteEmployeeById/"+id,{responseType:'text'});
+    return this.http.delete("http://100.26.50.117:9090/employee/deleteEmployeeById/"+id,{responseType:'text'});
   }
 
   changePassword(employeeRef:any){
-      return this.http.put("http://localhost:9090/employee/changePassword", employeeRef, { responseType: 'text' })
+      return this.http.put("http://100.26.50.117:9090/employee/changePassword", employeeRef, { responseType: 'text' })
   }
 
   retrieveAllEmployeeDetails(): Observable<Employee[]> {
-    return this.http.get<Employee[]>("http://localhost:9090/employee/allEmployeeDetails")
+    return this.http.get<Employee[]>("http://100.26.50.117:9090/employee/allEmployeeDetails")
   }
 }
--- Frontend/product-grocers/src/app/services/order.service.ts
+++ Frontend/product-grocers/src/app/services/order.service.ts
@@ -10,22 +10,22 @@ export class OrderService {
 
   constructor(public http: HttpClient) { }
   storeOrderDetailsInfo(orderRef: any): any {
-    return this.http.post("http://localhost:9090/order/storeOrderDetails", orderRef, { responseType: "text" })
+    return this.http.post("http://100.26.50.117:9090/order/storeOrderDetails", orderRef, { responseType: "text" })
   }
   retrieveAllOrderDetails(): Observable<Order[]> {
-    return this.http.get<Order[]>("http://localhost:9090/order/allOrderDetails")
+    return this.http.get<Order[]>("http://100.26.50.117:9090/order/allOrderDetails")
   }
 
   retrieveOrderById(id: any): Observable<Order[]> {
-    return this.http.get<Order[]>("http://localhost:9090/order/retrieveOrderById/" + id)
+    return this.http.get<Order[]>("http://100.26.50.117:9090/order/retrieveOrderById/" + id)
   }
 
   //by default all HttpClient method return type is observable with json format data. 
   deleteOrderById(id: any): any {
-    return this.http.delete("http://localhost:9090/order/deleteOrderById/" + id, { responseType: 'text' });
+    return this.http.delete("http://100.26.50.117:9090/order/deleteOrderById/" + id, { responseType: 'text' });
   }
 
   updateOrder(orderRef: any): any {
-    return this.http.put("http://localhost:9090/order/updateOrderStatus", orderRef, { responseType: 'text' })
+    return this.http.put("http://100.26.50.117:9090/order/updateOrderStatus", orderRef, { responseType: 'text' })
   }
 }
