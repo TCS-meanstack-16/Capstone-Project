3641311ce0b8076dd5f16caadfd930d665f799aa
mvaldez2 <miguel.valdez@valpo.edu>
2021-04-30 11:52:53 -0500

    deploy

--- Frontend/product-grocers/src/app/employee/employee.component.ts
+++ Frontend/product-grocers/src/app/employee/employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../services/employee.service';
 
 @Component({
   selector: 'app-employee',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent implements OnInit {
+  userID:number;
+  isAdmin:boolean = false;
+  isLoggedIn:boolean = false;
 
-  constructor() { }
+  constructor(public empSer: EmployeeService){ }
 
   ngOnInit(): void {
+    this.userID = JSON.parse((localStorage.getItem("userId")));
+    if(this.userID.toString() != ""){
+      this.isLoggedIn = true;
+    }
+    let employee = this.empSer.getEmployee(this.userID).subscribe(employee =>{
+      this.isAdmin = employee[0].isAdmin;
+    });
   }
 
 }
