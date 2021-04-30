7051a99c5729129d08b1c3bf16aa6ab641376441
Harshvardhan Sawal <hv.sawal@gmail.com>
2021-04-30 10:36:35 -0400

    Component UI changes for tables and forms

--- Frontend/product-grocers/src/app/shared/nav/nav.component.ts
+++ Frontend/product-grocers/src/app/shared/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   userID;
   isLoggedIn = false;
-  constructor() { }
+  constructor(public route: Router) { }
 
   ngOnInit(): void {
     this.userID = JSON.parse((localStorage.getItem("userId")));
@@ -20,6 +21,8 @@ export class NavComponent implements OnInit {
   logout(){
     localStorage.setItem("userId","")
     this.isLoggedIn = false;
+    this.route.navigate(['/login-page']);
+
   }
 
 }
